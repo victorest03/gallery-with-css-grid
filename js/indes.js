5ebe2bdbe53f3f7a141d4cfb1517f8c49b8c567d
Victor Manuel Estrada Llancary <elvictor-003@hotmail.com>
2022-05-03 21:00:13 -0500

    animation height

--- js/indes.js
+++ js/indes.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".group__title").forEach(element => {
     element.addEventListener("click", (e) => {
-        if(e.target.classList.contains("show")) e.target.classList.replace("show","hide")
-        else e.target.classList.replace("hide","show");
+        if(e.target.nextElementSibling.classList.contains("show")) e.target.nextElementSibling.classList.replace("show","hide")
+        else e.target.nextElementSibling.classList.replace("hide","show");
     });
 });
