581b54c24a54477cd6db4f7908456183a7161b4a
Victor Manuel Estrada Llancary <elvictor-003@hotmail.com>
2022-05-15 19:39:14 -0500

    add animation collapse tab

--- js/indes.js
+++ js/indes.js
@@ -1,6 +1,6 @@
-document.querySelectorAll(".group__title").forEach(element => {
-    element.addEventListener("click", (e) => {
-        if(e.target.nextElementSibling.classList.contains("show")) e.target.nextElementSibling.classList.replace("show","hide")
-        else e.target.nextElementSibling.classList.replace("hide","show");
-    });
-});
+// document.querySelectorAll(".group__title").forEach(element => {
+//     element.addEventListener("click", (e) => {
+//         if(e.target.nextElementSibling.classList.contains("show")) e.target.nextElementSibling.classList.replace("show","hide")
+//         else e.target.nextElementSibling.classList.replace("hide","show");
+//     });
+// });
